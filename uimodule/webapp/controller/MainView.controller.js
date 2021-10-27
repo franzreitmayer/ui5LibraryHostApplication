9a58d3f532bf905a976d2f6e457cf0c28d0e3387
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-10-27 22:46:59 +0200

    inital commit

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["eu/reitmayer/ui5/ui5LibraryHostApplication/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("eu.reitmayer.ui5.ui5LibraryHostApplication.controller.MainView", {});
+});
